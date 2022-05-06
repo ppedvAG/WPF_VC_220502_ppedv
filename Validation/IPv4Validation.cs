d8ca589b6e037533260c175ea296f753dcd03eec
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-05-06 12:05:01 +0200

    Validation

---
+++ Validation/IPv4Validation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Validation
+{
+    internal class IPv4Validation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (Regex.IsMatch(value.ToString(), @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"))
+                return ValidationResult.ValidResult;
+            else
+                return new ValidationResult(false, "Bitte gib eine IPv4-Adresse ein.");
+        }
+    }
+
+    public class ValidationRuleCollection : Collection<ValidationRule> { }
+
+    public class ValidationMarkup : MarkupExtension
+    {
+        private Binding binding;
+
+        public ValidationMarkup(Binding binding, ValidationRuleCollection validationRules)
+        {
+            this.binding = binding;
+
+            foreach (ValidationRule rule in validationRules)
+            {
+                binding.ValidationRules.Add(rule);
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}
