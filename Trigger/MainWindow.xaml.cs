4e58cfa83046c6711c125fe8b968f454c34938a2
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-05-09 12:27:23 +0200

    UserControls (kommentiert)

--- Trigger/MainWindow.xaml.cs
+++ Trigger/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserControls;
 
 namespace Trigger
 {
@@ -50,9 +51,23 @@ namespace Trigger
             BoolVal = !BoolVal;
         }
 
+        //EventHandler des UserControls (vgl. M11_UserControls)
         private void ColorPicker_Tap(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(ColorPicker.PickedColor.ToString());
+            MessageBox.Show((sender as ColorPicker).PickedColor.ToString());
+        }
+
+        private void ColorPicker_PickedColorChanged(object sender, RoutedEventArgs e)
+        {
+            if ((sender as ColorPicker).PickedColor.ToString().Equals("#FF000000"))
+                MessageBox.Show("Alles ist schwarz");
+        }
+
+        //EventHandler des Buttons
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Zugriff auf AttachedProperty
+            MessageBox.Show(ColorPicker.GetCount(sender as Button).ToString());
         }
     }
 }
