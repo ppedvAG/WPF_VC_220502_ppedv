b9007b0b3c1b679afe919a2640584ffea7a1be7a
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-05-11 10:27:11 +0200

    MVVM

---
+++ Commands/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Commands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OpenCmd = new CustomCommand
+                (
+                    p => new MainWindow().Show(),
+                    p => true
+                );
+
+            this.DataContext = this;
+        }
+
+        private void DeleteCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            (e.OriginalSource as TextBox).Text = "";
+        }
+
+        private void DeleteCmd_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty((e.OriginalSource as TextBox).Text);
+        }
+
+        public CloseCommand CloseCmd { get; set; } = new CloseCommand();
+
+        public CustomCommand OpenCmd { get; set; }
+
+        public static RoutedUICommand MyCmd { get; set; } = new RoutedUICommand("Mein Command", "My Command", typeof(MainWindow));
+    }
+}
---
+++ MVVM/ViewModel/StartViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM.ViewModel
+{
+    public class StartViewModel : INotifyPropertyChanged
+    {
+        public CustomCommand LadeDbCmd { get; set; }
+        public CustomCommand OeffneDbCmd { get; set; }
+
+        public int AnzahlPersonen { get { return Model.Person.Personenliste.Count; } }
+
+        public StartViewModel()
+        {
+            LadeDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        Model.Person.LadePersonenAusDb();
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnzahlPersonen)));
+                    },
+                    p => AnzahlPersonen == 0
+                );
+            OeffneDbCmd = new CustomCommand
+                (
+                    p =>
+                    {
+                        //TODO: Öffe DataGrid-Fenster
+
+                        (p as Window).Close();
+                    },
+                    p => AnzahlPersonen > 0
+                );
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
